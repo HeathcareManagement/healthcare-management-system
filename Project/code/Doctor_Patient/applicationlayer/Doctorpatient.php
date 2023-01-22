4a50c0c03625f08f916841b8a947d89abd0c4e6d
Charan Romi <90744611+charan2r@users.noreply.github.com>
2023-01-22 20:09:37 +0530

    Add files via upload

---
+++ Project/code/Doctor_Patient/applicationlayer/Doctorpatient.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Doctor Patient</title>
+	<link rel="stylesheet"  href="doctorpatient.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	
+</head>
+
+<body>
+
+</div>
+<div class="wrapper">
+		<div class="btn" > <a href="/Project/code/channel.html">
+
+<button type="button" class="buttonB" > Channelling</button>
+
+</div>
+	<div class="wrapper">
+		<div class="btn" > <a href="login3.php">
+
+<button type="button" class="buttonA" > Admin</button>
+
+</div>
+
+</div>
+
+
+	<div class="wrapper" >
+	<div class="btn" > <a href="login.php"  >
+<button type="button" class="buttonP">Patient</button>
+</div>
+
+</div>
+
+
+	<div class="wrapper" >
+	<div class="btn" > <a href="login2.php"  >
+<button type="button" class="buttonD">Doctor</button>
+        </a>
+</div>
+
+</div>
+
+	
+
+
+ 
+	
+
+
+
+
+</body>
+</html>
